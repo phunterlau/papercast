f38d43605567039ff6726d88b431220a2f4ce34b
Hongliang Liu <phunter.lau@gmailcom>
2024-09-02 19:19:48 -0700

    improved summarization and more stable voice generation

--- summarizer.py
+++ summarizer.py
@@ -1,6 +1,9 @@
 import yaml
 import os
 from llm_funcs import gen_gpt_chat_completion
+from arxiv_reader import get_arxiv, get_arxiv_id
+from pdf_reader import get_pdf
+import requests
 
 def load_system_prompt(yaml_file):
     with open(yaml_file) as file:
@@ -78,60 +81,92 @@ def third_pass(content, first_pass_summary, second_pass_summary):
     """
     return gen_gpt_chat_completion("", prompt.format(content=content, first_pass_summary=first_pass_summary, second_pass_summary=second_pass_summary), temp=0.7).choices[0].message.content
 
+def download_pdf(url, file_path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
+        return True
+    return False
+
 def generate_summary_arxiv(url='https://arxiv.org/abs/2405.04434', episode='1', use_cache=False,
                            prompt='dialogue_prompt', background_knowledge='None',
                            additional_research_questions="None"):
-    from arxiv_reader import get_arxiv
+    arxiv_id = get_arxiv_id(url)
+    if not arxiv_id:
+        print(f"Error: Could not extract arXiv ID from URL: {url}")
+        return None
+
     arxiv_dict = get_arxiv(url, use_cache=use_cache)
+    
+    # Check if HTML content is available
+    if not arxiv_dict or 'content' not in arxiv_dict or not arxiv_dict['content'].strip():
+        print(f"HTML content not available for {url}. Attempting to fetch PDF...")
+        # Construct PDF URL and local file path
+        pdf_url = f"https://arxiv.org/pdf/{arxiv_id}"
+        pdf_dir = 'pdfs'
+        if not os.path.exists(pdf_dir):
+            os.makedirs(pdf_dir)
+        pdf_path = os.path.join(pdf_dir, f"{arxiv_id}.pdf")
+        
+        if use_cache and os.path.exists(pdf_path):
+            print(f"Using cached PDF: {pdf_path}")
+        else:
+            print(f"Downloading PDF from {pdf_url}")
+            if not download_pdf(pdf_url, pdf_path):
+                print(f"Error: Failed to download PDF from {pdf_url}")
+                return None
+        
+        try:
+            arxiv_dict = get_pdf(pdf_path)
+        except Exception as e:
+            print(f"Error processing PDF: {str(e)}")
+            return None
+    
     prompt_dict = load_system_prompt('prompts.yaml')
 
+    # Check if arxiv_dict is empty or doesn't contain expected keys
+    if not arxiv_dict:
+        print(f"Error: Failed to retrieve data for URL: {url}")
+        return None
+
+    required_keys = ['title', 'authors', 'abstract', 'content']
+    missing_keys = [key for key in required_keys if key not in arxiv_dict]
+    if missing_keys:
+        print(f"Error: Data is missing the following required keys: {', '.join(missing_keys)}")
+        return None
+
+    # let the run YAML file specify the prompt to use
+    # so each run can specify dialogue style or monologue style, in English or Chinese
     generation_prompt = prompt_dict[prompt]
     research_question_prompt = prompt_dict['research_question_prompt']
     topic_prompt = prompt_dict['topic_prompt']
     background_knowledge_prompt = background_knowledge
     title = arxiv_dict['title']
     authors = arxiv_dict['authors']
+    # Convert authors to string if it's a list
+    if isinstance(authors, list):
+        authors = ', '.join(authors)
     abstract = arxiv_dict['abstract']
     content = arxiv_dict['content']
     dummy_content = 'Not available.'
-
-    # Perform three-pass analysis
-    first_pass_summary = first_pass(content)
-    second_pass_summary = second_pass(content, first_pass_summary)
-    third_pass_summary = third_pass(content, first_pass_summary, second_pass_summary)
-    
-    three_pass_summary = f"""
-    First Pass:
-    {first_pass_summary}
-
-    Second Pass:
-    {second_pass_summary}
-
-    Third Pass:
-    {third_pass_summary}
-    """
 
     topic_prediction = gen_gpt_chat_completion("", topic_prompt + '\n Title:' + title + '\nAbstract:' + abstract + '\n')
     generated_topic = topic_prediction.choices[-1].message.content.strip()
     print("article topic:", generated_topic)
     research_question_prompt = research_question_prompt.replace('<TITLE>', title).replace('<ABSTRACT>', abstract).replace('<TOPIC>', generated_topic).replace('<CONTENT>', dummy_content)
+    # generate the research question using title and abstract
+    # TODO: research questions from title and abstract may not be the best choice
+    # the full text with selected sections may be better
     research_questions = gen_gpt_chat_completion(research_question_prompt,'', temp=0.5).choices[-1].message.content.strip()
-    research_questions += "\nAdditional research questions:\n" + additional_research_questions
-    
-    summarizer_prompt = generation_prompt.replace('<EPISODE_NUMBER>', episode)
-    summarizer_prompt = summarizer_prompt.replace('<BACKGROUND_KNOWLEDGE>', background_knowledge_prompt)
-    summarizer_prompt = summarizer_prompt.replace('<TITLE>', title)
-    summarizer_prompt = summarizer_prompt.replace('<ABSTRACT>', abstract)
-    summarizer_prompt = summarizer_prompt.replace('<AUTHORS>', authors)
-    summarizer_prompt = summarizer_prompt.replace('<TOPIC>', generated_topic)
-    summarizer_prompt = summarizer_prompt.replace('<RESEARCH_QUESTIONS>', research_questions)
-    summarizer_prompt = summarizer_prompt.replace('<THREE_PASS_ANALYSIS>', three_pass_summary)
-    summarizer_prompt = summarizer_prompt.replace('<ORIGINAL_CONTENT>', content)
-    
-    summary = gen_gpt_chat_completion(summarizer_prompt, '', temp=0.7, max_tokens=4096).choices[-1].message.content.strip()
+    research_questions += "Additional research questions:\n" + additional_research_questions
+    # inject the generated topic, research questions etc
+    summarizer_prompt = generation_prompt.replace('<EPISODE_NUMBER>', episode).replace('<BACKGROUND_KNOWLEDGE>',background_knowledge_prompt).replace('<TITLE>', title).replace('<ABSTRACT>', abstract).replace('<AUTHORS>',authors).replace('<TOPIC>', generated_topic).replace('<RESEARCH_QUESTIONS>', research_questions)
+    # generate the summary with some high temperature for more creativity
+    summary = gen_gpt_chat_completion(summarizer_prompt, content, temp=0.7, max_tokens=4096).choices[-1].message.content.strip()
 
     return {
-        'arxiv_id': arxiv_dict['arxiv_id'],
+        'arxiv_id': arxiv_id,
         'title': title,
         'authors': authors,
         'abstract': abstract,
--- tts_gen.py
+++ tts_gen.py
@@ -5,32 +5,33 @@ import os
 import soundfile
 
 chat = ChatTTS.Chat()
-chat.load_models(compile=False) 
+chat.load_models(compile=False)
 
-def generate_seed(new_seed):
-    return {
-        "__type__": "update",
-        "value": new_seed
-        }
+# Load pre-trained speaker embeddings
+justin_spk = torch.load('seed_1509_restored_emb.pt', map_location=torch.device('cpu'))
+emma_spk = torch.load('seed_1742_restored_emb.pt', map_location=torch.device('cpu'))
 
-# core function to produce audio data for a given text
-# use the audio_seed_input to generate the speaker seed
-# use speech_speed to control the speed of the speech
-# use params_refine_text to make the text more podcast friendly
-def generate_audio(text, temperature, top_P, top_K, audio_seed_input, text_seed_input, 
+# Map speaker names to their embeddings
+speaker_embedding_map = {
+    "Justin": justin_spk,
+    "Emma": emma_spk
+}
+
+# Core function to produce audio data for a given text
+def generate_audio(text, temperature, top_P, top_K, speaker_name, text_seed_input, 
                    refine_text_flag, 
                    speech_speed = '[speed_1]',
                    params_refine_text = {'prompt': '[oral_1][laugh_0][break_5]'}):
 
-    torch.manual_seed(audio_seed_input)
-    rand_spk = chat.sample_random_speaker()
+    print(f"Generating audio for speaker: {speaker_name}")
+    spk_emb = speaker_embedding_map.get(speaker_name, justin_spk)  # Default to Justin if speaker not found
     params_infer_code = {
-        'spk_emb': rand_spk, 
+        'spk_emb': spk_emb, 
         'temperature': temperature,
         'prompt': speech_speed,
         'top_P': top_P,
         'top_K': top_K,
-        }
+    }
     
     torch.manual_seed(text_seed_input)
 
@@ -54,37 +55,36 @@ def generate_audio(text, temperature, top_P, top_K, audio_seed_input, text_seed_
 
     return [(sample_rate, audio_data), text_data]
 
-# wrapper function to generate audio data for a given text, speaker_seed and sequence_id
-def produce_audio_data(text, speaker_seed, sequence_id=1):
+# Wrapper function to generate audio data for a given text, speaker name and sequence_id
+def produce_audio_data(text, speaker_name, sequence_id=1):
     temperature = 0.7
     top_P = 0.3
     top_K = 20
-    audio_seed_input = speaker_seed
-    text_seed_input = 42
+    text_seed_input = 43
     refine_text_flag = True
 
-    (sample_rate,audio), refined_text = generate_audio(text, temperature, top_P, top_K, audio_seed_input, text_seed_input, refine_text_flag)
+    (sample_rate, audio), refined_text = generate_audio(text, temperature, top_P, top_K, speaker_name, text_seed_input, refine_text_flag)
 
-    # calculate the audio duration in seconds 
+    # Calculate the audio duration in seconds 
     audio_duration = len(audio) / sample_rate
     return audio, refined_text, audio_duration
 
-# given a list of text, speaker_seed and sequence_id, 
+# Given a list of text, speaker names and sequence_id, 
 # produce audio files for each speaker and save them to a single audio file
 def produce_audio(transcript, audio_filename = 'test.wav', offset=0):
     audio_data = []
     transcript_text = [text for text, _, _ in transcript]
-    for text, speaker_seed, sequence_id in transcript:
-        audio, refined_text, audio_duration = produce_audio_data(text, speaker_seed, sequence_id)
+    for text, speaker_name, sequence_id in transcript:
+        audio, refined_text, audio_duration = produce_audio_data(text, speaker_name, sequence_id)
         audio_data.append((audio, refined_text, audio_duration))
-        print("finished producing audio for sequence_id:", sequence_id)
+        print(f"finished producing audio for sequence_id: {sequence_id}, speaker: {speaker_name}")
         print(audio_duration, refined_text)
     
     audio_data_all, refined_text_all, audio_duration_all = zip(*audio_data)
     audio_data_all = np.concatenate(audio_data_all)
     sample_rate = 24000
 
-    # generate the srt subtitle file with the audio duration from the transcript
+    # Generate the srt subtitle file with the audio duration from the transcript
     srt_list = []
     if offset > 0: # from zero to offset time, there is music, just show [MUSIC]
         start_time_str = "{:02d}:{:02d}:{:02d},{:03d}".format(0, 0, 0, 0)
@@ -95,10 +95,10 @@ def produce_audio(transcript, audio_filename = 'test.wav', offset=0):
     for text, audio_duration in zip(transcript_text, audio_duration_all):
         print(text, audio_duration)
         end_time = start_time + audio_duration
-        # convert the start and end time to the srt format
+        # Convert the start and end time to the srt format
         start_time_str = "{:02d}:{:02d}:{:02d},{:03d}".format(int(start_time//3600), int((start_time//60)%60), int(start_time%60), int((start_time*1000)%1000))
         end_time_str = "{:02d}:{:02d}:{:02d},{:03d}".format(int(end_time//3600), int((end_time//60)%60), int(end_time%60), int((end_time*1000)%1000))
-        # output the srt format
+        # Output the srt format
         print(start_time_str, '-->', end_time_str)
         time_str = start_time_str + ' --> ' + end_time_str
         srt_list.append((time_str, text))
@@ -107,28 +107,27 @@ def produce_audio(transcript, audio_filename = 'test.wav', offset=0):
     if not os.path.exists('audio'):
         os.makedirs('audio')
     
-    # save the srt file
-    with open('audio/subtitle_'+audio_filename+'.srt', 'w') as srt_file:
+    # Save the srt file
+    with open(f'audio/subtitle_{audio_filename}.srt', 'w') as srt_file:
         for i, (time_str, text) in enumerate(srt_list):
-            srt_file.write(str(i+1) + '\n')
-            srt_file.write(time_str + '\n')
-            srt_file.write(text + '\n\n')
+            srt_file.write(f"{i+1}\n")
+            srt_file.write(f"{time_str}\n")
+            srt_file.write(f"{text}\n\n")
         
-    # soundfile is a more friendly lib for concatenated audio in numpy array
-    # ChatTTS's torch audio example was not working for me
-    soundfile.write("audio/"+audio_filename, audio_data_all, sample_rate)
+    # Soundfile is a more friendly lib for concatenated audio in numpy array
+    soundfile.write(f"audio/{audio_filename}", audio_data_all, sample_rate)
 
-    with open('audio/refined_text_'+audio_filename+'.txt', 'w') as file:
+    with open(f'audio/refined_text_{audio_filename}.txt', 'w') as file:
         for refined_text in refined_text_all:
-            file.write(refined_text + '\n')
+            file.write(f"{refined_text}\n")
 
-    print("audio file saved to audio/" + audio_filename
-          + " and refined text saved to audio/refined_text_"+audio_filename+".txt")
+    print(f"Audio file saved to audio/{audio_filename} and refined text saved to audio/refined_text_{audio_filename}.txt")
     return refined_text_all
 
 if __name__ == '__main__':
-    text = ("Reinforcement Learning (RL) further aligns the model with human preferences by optimizing responses based on reward models."
-            "In DeepSeek-V2, RL is applied in two stages: first focusing on reasoning capabilities and then on general human preference alignment."
-            "This approach ensures the model performs well in both specific tasks like coding and math, and in general conversational contexts.")
-    speaker_seed = 2400
-    refined_text = produce_audio(text, speaker_seed)
+    test_transcript = [
+        ("Reinforcement Learning, or RL, further aligns the model with human preferences by optimizing responses based on reward models.", "Justin", 1),
+        ("In DeepSeek-V2, RL is applied in two stages: first focusing on reasoning capabilities and then on general human preference alignment.", "Emma", 2),
+        ("This approach ensures the model performs well in both specific tasks like coding and math, and in general conversational contexts.", "Justin", 3)
+    ]
+    refined_text = produce_audio(test_transcript, "test_output.wav")
